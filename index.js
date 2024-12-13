ed178eb3e4ef110d1faef4b78a7f0719fa9b343d
Souravroy99 <souravroy200418@gmail.com>
2024-12-14 01:27:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+ 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD') ;
+})
+
+app.get('/twt', (req, res) => {
+    res.send('TWITTER') ;
+}) 
+
+app.get('/login', (req, res) => {
+    res.send('<h1>LOGIN</h1>') ;
+})
+
+const PORT = process.env.PORT ;
+app.listen(PORT, () => {
+    console.log(`Server is running at: ${PORT}`)
+})
